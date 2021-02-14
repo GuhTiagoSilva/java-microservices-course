ab63a91ab412423a93e7990f7d846e9eb3ac082d
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2021-02-14 16:12:19 -0500

    Ribbon Loading Balance

--- hr-payroll/src/main/java/com/gustavo/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/gustavo/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.gustavo.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients // Basic Configuration to Feign works
 @SpringBootApplication
 public class HrPayrollApplication {
