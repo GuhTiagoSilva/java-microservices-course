d0e39f4943b8864881b089831bb93f043233654e
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2021-02-14 15:11:15 -0500

    Project hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/gustavo/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.gustavo.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.gustavo.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.00, days);
+	}
+
+}
