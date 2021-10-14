22ecedc535795c96c5f543e92037ff5cb2ba9bd2
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-10-14 20:38:47 -0300

    Database Seeding

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,15 +1,20 @@
 package com.devsuperior.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
-
 }
